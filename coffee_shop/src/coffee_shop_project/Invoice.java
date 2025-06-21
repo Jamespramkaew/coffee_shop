e7a90fdbaf01800b23b37c6ba204955751f0bdfc
Jamespramkaew <jamespramkaew88@gmail.com>
2025-06-22 00:44:14 +0700

    Modify Invoice Class that decorate a getDisplay method

--- coffee_shop/src/coffee_shop_project/Invoice.java
+++ coffee_shop/src/coffee_shop_project/Invoice.java
@@ -12,14 +12,24 @@ public class Invoice {
 	}
 	
 	public void getDisplay() {
+		int n = 0;
+		double result = 0.0;
+		System.out.println("==================================");
+		System.out.printf("%s %6s %12s %7s\n","Index","List","Qty","Price");
 		for (Order i : this.order) {
-			System.out.printf("%8s %5.4f N: %d %5.4f\n",
+			System.out.printf("%3d %8s %8.2f  %d  %6.2f\n",
+					n++,
 					i.getItem().getName(),
 					i.getItem().getPrice(),
 					i.getNumber(),
 					i.getRP()
 					);
+			
+			result = result + i.getResultP();
 		}
+		System.out.println("----------------------------------");
+		System.out.printf("%25s %6.2f\n","Totle Price:", result);
+		System.out.println("==================================\n");
 	}
 
 	public static void main(String[] args) {
@@ -29,17 +39,25 @@ public class Invoice {
 		Item tea = new Item("Tea", 0.19);
 		Item dessert = new Item("coffee", 1.69);
 		
+		//User's Address
 		Invoice inv = new Invoice(
 				"Mr.J", 
 				"1/38 Cha-am ,Phatchburi 76120"
 				);
 		
+		//Order Here
 		inv.order.add(new Order(coffee,2));
 		inv.order.add(new Order(tea,1));
 		inv.order.add(new Order(dessert,3));
 		
+		
 		inv.getDisplay();
-
+		
+		inv.order.remove(2);
+		inv.order.add(new Order(dessert,1));
+		
+		inv.getDisplay();
+		//Order Here
 	}
 
 }
