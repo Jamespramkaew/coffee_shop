f9c750d8a226d023372bc20e60ae3d8f0da94f6c
Jamespramkaew <jamespramkaew88@gmail.com>
2025-06-21 21:55:29 +0700

    Update detail of Order & Invoice class and can choose item on ArrayList

--- coffee_shop/src/coffee_shop_project/Invoice.java
+++ coffee_shop/src/coffee_shop_project/Invoice.java
@@ -3,9 +3,33 @@ package coffee_shop_project;
 import java.util.ArrayList;
 
 public class Invoice {
+	private Person p;
+	private ArrayList<Order> order;
+	
+	public Invoice(String name, String addrs) {
+		this.p = new Person(name, addrs);
+		this.order = new ArrayList();
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//List of shop
+		Item coffee = new Item("Coffee", 0.59);
+		Item tea = new Item("Tea", 0.19);
+		Item dessert = new Item("coffee", 1.69);
+		
+		Invoice inv = new Invoice(
+				"Mr.J", 
+				"1/38 Cha-am ,Phatchburi 76120"
+				);
+		
+		inv.order.add(new Order(coffee,2));
+		inv.order.add(new Order(tea,1));
+		inv.order.add(new Order(dessert,3));
+		
+//		for (Order i : inv.order) {
+//			System.out.println(i.getResultP());
+//		}
 
 	}
 
--- coffee_shop/src/coffee_shop_project/Order.java
+++ coffee_shop/src/coffee_shop_project/Order.java
@@ -3,5 +3,20 @@ package coffee_shop_project;
 public class Order {
 	private Item item;
 	private int number;
-	private double resultPrice;
+	private double resultPrice = 0.0;
+	
+	public Order(Item itm, int n) {
+		this.item = itm;
+		this.number = n;
+		this.addItem();
+	}
+	
+	public void addItem() {
+		this.resultPrice = this.number * this.item.getPrice();
+	}
+	
+	//Test
+	public double getResultP() {
+		return this.resultPrice;
+	}
 }
