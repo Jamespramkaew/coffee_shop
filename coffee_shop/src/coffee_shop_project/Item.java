ce1467fec7c513d02470266cfab6d3927b6582fc
Jamespramkaew <jamespramkaew88@gmail.com>
2025-06-21 20:53:02 +0700

    Update adding Constructor and get method for both Item & Person class

--- coffee_shop/src/coffee_shop_project/Item.java
+++ coffee_shop/src/coffee_shop_project/Item.java
@@ -3,4 +3,17 @@ package coffee_shop_project;
 public class Item {
 	private String name;
 	private double price;
+	
+	public Item(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public double getPrice() {
+		return this.price;
+	}
 }
--- coffee_shop/src/coffee_shop_project/Person.java
+++ coffee_shop/src/coffee_shop_project/Person.java
@@ -4,5 +4,16 @@ public class Person {
 	private String name;
 	private String Address;
 	
+	public Person(String name, String addrs) {
+		this.name = name;
+		this.Address = addrs;
+	}
 	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getAddrs() {
+		return this.Address;
+	}
 }
